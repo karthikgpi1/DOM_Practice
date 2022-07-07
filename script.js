ac114eb805d22fa1ff05253d8eec89c4caa769d9
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-07 09:45:19 -0700

    Replace and set attributes using dom

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-const heading = document.getElementsByTagName('h1')[0];
+// const heading = document.getElementsByTagName('h1')[0];
 
-const cool = document.getElementsByTagName('h1')[1];
+// const cool = document.getElementsByTagName('h1')[1];
 
-console.log(heading.innerText);
+// console.log(heading.innerText);
 
-console.log(cool.innerText);
+// console.log(cool.innerText);
+
+
+
+// const maincontent = document.getElementsByClassName('content')[0];
+
+// console.log(maincontent);
+
+// console.log(maincontent.innerText);
+
+
+
+// const buttoncontent = document.getElementById('attention');
+
+// console.log(buttoncontent.innerText);
+
+
+
+
+// // it can do all these above
+
+
+// const btncontent = document.querySelector('#attention');
+// console.log(btncontent.innerText);
+
+// const allh1s = document.querySelectorAll("h1");
+// const firsth1 =allh1s[0];
+
+// console.log(firsth1.innerText);
+// console.log(document.querySelectorAll(".content")[0].innerTex)
+
+
+//---------------replace content---------------------
+
+// const mainContent = document.querySelector('.content');
+
+// mainContent.innerText = 'nice to see you!';
+
+//--------------set attributes-----
+
+// const head2 = document.querySelectorAll('h1')[1];
+
+// head2.setAttribute('class', 'red');
+
