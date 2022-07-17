5b0fa87fbf4bad1167d5c4f6e5b03caee6d9b33d
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-17 12:51:33 +0530

    async/await example using mock api

--- script.js
+++ script.js
@@ -358,26 +358,73 @@
 
 //-------async/await example--------------
 
-const drive = (age) => new Promise((resolve, reject) => {          //use capitalize like Promise
+// const drive = (age) => new Promise((resolve, reject) => {          //use capitalize like Promise
     
 
-    if (age >=18) {
-        resolve('eligible to drive');
-    }else{
-        reject('not eligible to drive');
-    }
-});
+//     if (age >=18) {
+//         resolve('eligible to drive');
+//     }else{
+//         reject('not eligible to drive');
+//     }
+// });
 
-async function niceMessage(){
-  try {
-        const msg = await drive(12);
-        console.log(msg);
-    }catch(errmsg){
-        console.log(errmsg);
-    }
+// async function niceMessage(){
+//   try {
+//         const msg = await drive(12);
+//         console.log(msg);
+//     }catch(errmsg){
+//         console.log(errmsg);
+//     }
+// }
+
+// niceMessage();
+
+//--------------------------------
+
+// fetch(`https://62d25db3afb0b03fc5a57872.mockapi.io/users`)
+//     .then((resp) => resp.json())
+//     .then((users) => {
+//         users.forEach((user) => {            
+//             createUser(user)
+//             console.log(user);
+//         }); 
+//     })
+//     .catch((err) => console.log(err));
+
+//--------------async/await example using mock api--------------------------------------
+async function getusers(){
+  const data = await fetch(`https://62d25db3afb0b03fc5a57872.mockapi.io/users`);
+  const users = await data.json();
+  console.log(users);
+
+  users.forEach(user => createUser(user));
+}
+//-----------------------
+
+  function createUser ({name, createdAt, avatar,id}) {
+const info = document.createElement("div");
+info.setAttribute("class", "container");
+
+info.innerHTML = `
+<div class ="container1">
+    <div class = "user-container">
+      <img class ="user" src =${avatar} width = "250px" height = "150px">
+    </div>
+    
+    <div class = "details">
+      <h4> ${name} </h4>
+      <p>${createdAt}</p>
+    </div>
+    <div class = "id">
+      <p>ID:${id}</p>
+    </div>
+</div>`; 
+
+document.body.append(info);
 }
 
-niceMessage();
+getusers();
+
 
 
 
